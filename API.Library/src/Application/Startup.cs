6c895073c58f8df718ba793724c631474741caba
Axiom Axiom <axiommanagementsystem@outlook.com>
2024-04-27 23:46:47 +0100

    added project references with test project

---
+++ API.Library/src/Application/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace Application
+{
+    public static class Startup
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            return services
+                .AddValidatorsFromAssembly(assembly)
+                .AddMediatR(assembly);
+        }
+    }
+}
---
+++ Test.LibraryAPI/LibraryAPI.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace LibraryAPI.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
